74f49684038e73511431c936ba5e083c353faab1
kapigoku <gomez.ramon2004@gmail.com>
2022-12-22 15:10:58 -0700

    Se optimizó el flujo de trabajo

--- Code01/main.py
+++ Code01/main.py
@@ -1,7 +1,5 @@
-from convertToTuple import *
-from maxAmountOfCaloriesByTotal import *
+from functions import *
 
 #Test
-tuple00 = ((1000,2000,3000), (4000,),(5000,6000), (7000,8000,9000), (10000,))
-tuple01 = convertToTuple()
-print(f"The person in the position {mostCaloriesIndex(tuple01)} has gotten over {mostCalories(tuple01)} calories.")
+listTest = [[1000,2000,3000], [4000,],[5000,6000], [7000,8000,9000], [10000,]]
+print(f"The person in the position {mostCaloriesIndex(convertToList())} has gotten over {mostCalories(totalCalories(convertToList()))} calories.")
