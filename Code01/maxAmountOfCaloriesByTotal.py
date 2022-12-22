df245594f8c30b8245ba21e845b63fb146701ae7
kapigoku <gomez.ramon2004@gmail.com>
2022-12-22 15:14:12 -0700

    15:13 - 22/12/22 : Ultima Actualización

--- Code01/maxAmountOfCaloriesByTotal.py
+++
@@ -1,6 +0,0 @@
-#mostCalories = lambda tupleCalories : max([sum(i) for i in tupleCalories])
-
-#MaxAmountOfCaloriesByTotal
-totalCalories = lambda tupleCalories : [sum(i) for i in tupleCalories]
-mostCalories = lambda tupleCalories : max(totalCalories(tupleCalories))
-mostCaloriesIndex = lambda tupleCalories : totalCalories(tupleCalories).index(mostCalories(tupleCalories))
