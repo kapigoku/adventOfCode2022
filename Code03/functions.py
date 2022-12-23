dcdebd292e7310dd8f37467f2974ac33df372aa5
kapigoku <gomez.ramon2004@gmail.com>
2022-12-22 22:57:00 -0700

    Se agregó splitBy3 y repeatedCharBy3 para nuevas funcionalidades

--- Code03/functions.py
+++ Code03/functions.py
@@ -3,8 +3,8 @@ def convertToList():
     return open('test03.txt', 'r').read().splitlines()
 
 
-#splitCompartment(stringList): Create a nested list, where each string it will split in two parts.
-def splitCompartment(stringList):
+#splitByLine(stringList): Create a nested list, where each string it will split in two parts.
+def splitByLine(stringList):
 
     splittedList = [[j for j in range(2)] for i in range(len(stringList))]
 
@@ -16,13 +16,30 @@ def splitCompartment(stringList):
     return splittedList
 
 
-#repeatedChar(stringList): Check which character is repeated, then it will append to a new list.
-def repeatedChar(splittedList):
+#splitBy3(stringList): Create a nested list, where every three lines it will append to a new list.
+splitBy3 = lambda stringList : [[stringList[j+(i*3)] for j in range(3)] for i in range(len(stringList)//3)]
+print(splitBy3(convertToList()))
+
+
+#repeatedCharByLine(stringList): Check which character is repeated, then it will append to a new list.
+def repeatedCharByLine(splittedList):
 
     newList = []
 
     for i in range(len(splittedList)):
         count = list(set(splittedList[i][0])&set(splittedList[i][1]))
+        for j in count:
+            newList.append(j)
+    return newList
+
+
+#repeatedCharBy3(stringList): Check which character is repeated, then it will append to a new list.
+def repeatedCharBy3(splittedList):
+
+    newList = []
+
+    for i in range(len(splittedList)):
+        count = list(set(splittedList[i][0])&set(splittedList[i][1])&set(splittedList[i][2]))
         for j in count:
             newList.append(j)
     return newList
