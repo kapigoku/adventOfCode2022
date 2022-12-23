e684944a9792937ad6373a6e40df303bcc72d657
kapigoku <gomez.ramon2004@gmail.com>
2022-12-22 21:39:08 -0700

    Se creó archivo

---
+++ Code03/main.py
@@ -0,0 +1,5 @@
+from functions import *
+
+#Test
+testList = ['vJrwpWtwJgWrhcsFMMfFFhFp','PmmdzqPrVvPwwTWBwg','wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn','ttgJtRGJQctTZtZT','CrZsJsPPZsGzwwsLwLmpwMDw']
+print(f"Total Score : {totalScore(priorityItem(repeatedChar(splitCompartment(convertToList()))))}")
