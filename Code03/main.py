5074faf705bedf5fce4ef4dde4476de29bf3a73f
kapigoku <gomez.ramon2004@gmail.com>
2022-12-22 22:57:47 -0700

    Se modificó interfaz para nuevas funcionaldiades

--- Code03/main.py
+++ Code03/main.py
@@ -2,4 +2,5 @@ from functions import *
 
 #Test
 testList = ['vJrwpWtwJgWrhcsFMMfFFhFp','PmmdzqPrVvPwwTWBwg','wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn','ttgJtRGJQctTZtZT','CrZsJsPPZsGzwwsLwLmpwMDw']
-print(f"Total Score : {totalScore(priorityItem(repeatedChar(splitCompartment(convertToList()))))}")
+print(f"Total Score by Splitted Line : {totalScore(priorityItem(repeatedCharByLine(splitByLine(convertToList()))))}")
+print(f"Total Score by Group : {totalScore(priorityItem(repeatedCharBy3(splitBy3(convertToList()))))}")
