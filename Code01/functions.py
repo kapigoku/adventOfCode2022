999fc0d1835e1805146a46d20a3a1a9d62b7b47d
kapigoku <gomez.ramon2004@gmail.com>
2022-12-24 15:04:06 -0700

    Se ajustaron ciertos elementos

--- Code01/functions.py
+++ Code01/functions.py
@@ -1,7 +1,7 @@
 #convertToList(): Convert text file to list, nesting a new list for each blank line.
 def convertToList():
-    with open('Code01/test01.txt') as temp:
-        return [list(map(int, item.split("\n"))) for item in temp.read().strip().split("\n\n")]
+    with open('Code01/test01.txt') as text:
+        return [list(map(int, item.split("\n"))) for item in text.read().strip().split("\n\n")]
 
 #mostCaloriesTotal : Sum each element from a nested list and then check the maximum calories.
-mostCaloriesTotal = lambda tupleCalories : max([sum(i) for i in tupleCalories])
+mostCaloriesTotal = lambda integerList : max([sum(i) for i in integerList])
