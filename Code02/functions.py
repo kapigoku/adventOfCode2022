0a136ba7a7a8ef4f3878953a3001f683c8a667a6
kapigoku <gomez.ramon2004@gmail.com>
2022-12-24 15:05:00 -0700

    Se optimizaron las primeras 3 funciones

--- Code02/functions.py
+++ Code02/functions.py
@@ -1,33 +1,23 @@
 #convertToList(): Convert text file to list, Nesting a new list each " ".
 def convertToList():
-    return [i.split() for i in open('test02.txt', 'r').read().splitlines()]
-
+    with open('Code02/test02.txt') as text:
+        return [line.split() for line in text]
 
 #changeFirstIndex(stringList): Convert the first index from each list to its equivalent from a dictionary variable.
 def changeFirstIndex(stringList):
-
     dictFile = {'A' : 'X', 'B' : 'Y', 'C' : 'Z',}
-
-    for i in range(len(stringList)):
-        stringList[i][0] = dictFile.get(stringList[i][0])
-
-    return stringList
-
+    return [[dictFile.get(item[0], item[0]) for item in sublist] for sublist in stringList]
 
 #outcomeRound(stringList): Create a new nested list based from the game map and matches matrix data.
 def outcomeRound(stringList):
-
     gameMap = {'X' : 0, 'Y' : 1, 'Z' : 2}
     wldMatrix = [[3, 6, 0], [0, 3, 6], [6, 0, 3]]
-    newList = [[j for j in range(2)] for i in range(len(stringList))]
 
-    for i in range(len(stringList)):
-        for j in range(2):
-            newList[i][0] = wldMatrix[gameMap.get(stringList[i][0])][gameMap.get(stringList[i][1])]
-            newList[i][1] = gameMap.get(stringList[i][1]) + 1
+    for item, subList in enumerate(stringList):
+        stringList[item][0] = wldMatrix[gameMap.get(subList[0])][gameMap.get(subList[1])]
+        stringList[item][1] = gameMap.get(subList[1]) + 1
 
-    return newList
-
+    return stringList
 
 #totalScore(stringList): Sum the nested list, to return the total score.
-totalScore = lambda integerList : sum([sum(i) for i in integerList])
+totalScore = lambda integerList : sum([sum(item) for item in integerList])
