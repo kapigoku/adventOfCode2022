32fd840d3b53ebe67e3c907084537b794e07c67b
kapigoku <gomez.ramon2004@gmail.com>
2022-12-24 14:01:56 -0700

    Se actualizó directorio de test01.txt

--- Code01/functions.py
+++ Code01/functions.py
@@ -1,6 +1,6 @@
 #convertToList(): Convert text file to list, nesting a new list for each blank line.
 def convertToList():
-    with open('test01.txt') as temp:
+    with open('Code01/test01.txt') as temp:
         return [list(map(int, item.split("\n"))) for item in temp.read().strip().split("\n\n")]
 
 #mostCaloriesTotal : Sum each element from a nested list and then check the maximum calories.
