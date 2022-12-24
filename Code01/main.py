6e3866e8bf3e4eecad3410b0dfa40f87c2bc1af1
kapigoku <gomez.ramon2004@gmail.com>
2022-12-23 21:41:54 -0700

    Se adaptó main.py para las nuevas funciones

--- Code01/main.py
+++ Code01/main.py
@@ -2,4 +2,4 @@ from functions import *
 
 #Test
 listTest = [[1000,2000,3000], [4000,],[5000,6000], [7000,8000,9000], [10000,]]
-print(f"The person in the position {mostCaloriesIndex(convertToList())} has gotten over {mostCalories(totalCalories(convertToList()))} calories.")
+print(f"Total Score : {mostCaloriesTotal(convertToList())}")
