8757c07cae36951066a928959c2a139501d11e89
kapigoku <gomez.ramon2004@gmail.com>
2022-12-23 21:40:12 -0700

    Se optimizó la función de convertToList(). Se desechó dos funciones redundantes y sin propósito

--- Code01/functions.py
+++ Code01/functions.py
@@ -1,29 +1,9 @@
-#convertToList(): Convert text file to list, Nesting a new list each " ".
+#convertToList(): Convert text file to list, nesting a new list for each " ".
 def convertToList():
-    
-    newList = list()
-    temp = list()
-    for i in open('test01.txt','r').read().splitlines():
-        if(i == ''):
-            newList.append(temp)
-            temp=list()
-        else:
-            temp.append(int(i))
-    
-    return newList
+    with open('test01.txt') as temp:
+        textList = temp.read().strip()
+        return [[int(sublist) for sublist in list.split("\n")] for list in textList.split("\n\n")]
 
 
 #mostCaloriesTotal : Sum each element from a nested list and then check the maximum calorie
-## mostCaloriesTotal = lambda tupleCalories : max([sum(i) for i in tupleCalories]) ##
-
-
-#totalCalories: Sum each element from a nested list.
-totalCalories = lambda listCalories : [sum(i) for i in listCalories]
-
-
-#mostCalories: Check the maximum calorie from a list.
-mostCalories = lambda listCalories :max(listCalories)
-
-
-#mostCaloriesIndex: Check the index from the maximum calorie from a list.
-mostCaloriesIndex = lambda listCalories : totalCalories(listCalories).index(mostCalories(totalCalories(listCalories)))
+mostCaloriesTotal = lambda tupleCalories : max([sum(i) for i in tupleCalories])
