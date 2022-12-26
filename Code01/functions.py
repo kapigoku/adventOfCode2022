3684bb1525e80cb7a67c104f020d1f64dfd3caab
kapigoku <gomez.ramon2004@gmail.com>
2022-12-26 15:33:10 -0700

    15:32 - 26/12/22 : Uso de buenas prácticas

--- Code01/functions.py
+++ Code01/functions.py
@@ -1,7 +1,9 @@
-#convertToList(): Convert text file to list, nesting a new list for each blank line.
+#convertToList(): Convert text file to a nested list of integers.
 def convertToList():
-    with open('Code01/test01.txt') as text:
-        return [list(map(int, string.split("\n"))) for string in text.read().strip().split("\n\n")]
+    with open('Code01/input.txt') as file:
+        blockIntStrs = file.read().split('\n\n')
+        return [[int(intStr) for intStr in intStrs.split("\n")] for intStrs in blockIntStrs]
 
 #mostCaloriesTotal : Sum each element from a nested list and then check the maximum calories.
-mostCaloriesTotal = lambda integerList : max([sum(subList) for subList in integerList])
+def mostCaloriesTotal(intLst):
+    return max([sum(intNestedLst) for intNestedLst in intLst])
--- Code01/main.py
+++ Code01/main.py
@@ -1,5 +1,5 @@
 from functions import *
 
 #Test
-listTest = [[1000,2000,3000], [4000,],[5000,6000], [7000,8000,9000], [10000,]]
+testLst= [[1000,2000,3000], [4000,],[5000,6000], [7000,8000,9000], [10000,]]
 print(f"Total Score : {mostCaloriesTotal(convertToList())}")
