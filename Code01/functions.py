8a4a58faf199ad3d0ab960f963aee82cafe35bac
kapigoku <gomez.ramon2004@gmail.com>
2022-12-26 15:45:31 -0700

    15:45 - 26/12/22 : Uso de buenas prácticas

--- Code01/functions.py
+++ Code01/functions.py
@@ -1,8 +1,8 @@
 #convertToList(): Convert text file to a nested list of integers.
 def convertToList():
     with open('Code01/input.txt') as file:
-        blockIntStrs = file.read().split('\n\n')
-        return [[int(intStr) for intStr in intStrs.split("\n")] for intStrs in blockIntStrs]
+        numStrBlock = file.read().split('\n\n')
+        return [[int(num) for num in numStr.split("\n")] for numStr in numStrBlock]
 
 #mostCaloriesTotal : Sum each element from a nested list and then check the maximum calories.
 def mostCaloriesTotal(intLst):
