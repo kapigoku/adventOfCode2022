2af7a1e6be0b379a6bb0c5889eb071b11d76a42b
kapigoku <gomez.ramon2004@gmail.com>
2022-12-26 11:54:18 -0700

    Optimización en general

--- Code02/functions.py
+++ Code02/functions.py
@@ -6,18 +6,18 @@ def convertToList():
 #changeFirstIndex(stringList): Convert the first index from each list to its equivalent from a dictionary variable.
 def changeFirstIndex(stringList):
     dictFile = {'A' : 'X', 'B' : 'Y', 'C' : 'Z',}
-    return [[dictFile.get(item[0], item[0]) for item in sublist] for sublist in stringList]
+    return [[dictFile.get(letter[0], letter[0]) for letter in sublist] for sublist in stringList]
 
 #outcomeRound(stringList): Create a new nested list based from the game map and matches matrix data.
-def outcomeRound(stringList):
+def outcomeRound(gameList):
     gameMap = {'X' : 0, 'Y' : 1, 'Z' : 2}
     wldMatrix = [[3, 6, 0], [0, 3, 6], [6, 0, 3]]
 
-    for item, subList in enumerate(stringList):
-        stringList[item][0] = wldMatrix[gameMap.get(subList[0])][gameMap.get(subList[1])]
-        stringList[item][1] = gameMap.get(subList[1]) + 1
+    for item, subList in enumerate(gameList):
+        gameList[item][0] = wldMatrix[gameMap.get(subList[0])][gameMap.get(subList[1])]
+        gameList[item][1] = gameMap.get(subList[1]) + 1
 
-    return stringList
+    return gameList
 
 #totalScore(stringList): Sum the nested list, to return the total score.
 totalScore = lambda integerList : sum([sum(item) for item in integerList])
