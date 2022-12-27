c6c339bd9234c51b7834bdfca71a8a18e2e00721
kapigoku <gomez.ramon2004@gmail.com>
2022-12-26 18:36:25 -0700

    Optimización y mejores prácticas

--- Code01/functions.py
+++ Code01/functions.py
@@ -1,9 +1,9 @@
 #convertToList(): Convert text file to a nested list of integers.
 def convertToList():
     with open('Code01/input.txt') as file:
-        numStrBlock = file.read().split('\n\n')
-        return [[int(num) for num in numStr.split("\n")] for numStr in numStrBlock]
+        lineBlockLst = file.read().split('\n\n')
+        return [[int(line) for line in lineBlock.split("\n")] for lineBlock in lineBlockLst]
 
 #mostCaloriesTotal : Sum each element from a nested list and then check the maximum calories.
 def mostCaloriesTotal(intLst):
-    return max([sum(intNestedLst) for intNestedLst in intLst])
+    return max([sum(intSubLst) for intSubLst in intLst])
